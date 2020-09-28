3647a10a1787314505a3b76d3db51b5273d47789
tleach01 <taylorleach0@gmail.com>
2020-09-28 07:29:56 -0400

    changing views and templates around

--- project/forms.py
+++ project/forms.py
@@ -1,3 +1,10 @@
 from django.forms import Form, ModelForm, CharField, ChoiceField
-from .models import Flashcard
+from .models import Flashcards
 
+class DeckForm(ModelForm):
+    class Meta:
+        model = Flashcards
+        fields = [
+            'front',
+            'back',
+        ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.messages import success, error
 from .models import Flashcards
-# from .forms import FrontCard, BackCard
+from .forms import DeckForm
 
 def deck_list(request):
     decks = Flashcards.objects.all()
@@ -9,3 +10,17 @@ def deck_list(request):
 def deck_detail(request):
     deck = get_object_or_404(Flashcards, pk=pk)
     return render(request, "project/deck_detail.html", {"deck": deck})
+
+
+def create_deck(request):
+    if request.method == "GET":
+        deck = DeckForm()
+    else:
+        deck = DeckForm(data=request.POST)
+
+        if deck.is_valid():
+            deck.save()
+
+            success(request, "New deck created.")
+            return redirect(to='deck_list')
+    return render(request, "decks/create_deck.html", {"deck": deck})
