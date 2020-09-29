d848459bc3680ad5e6abdb01b052dd109f5238e6
LoxductheGreat <jameelsalhhadin@gmail.com>
2020-09-29 11:00:47 -0400

    edited files

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
-# Create your models here.
+class Deck(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Flashcard(models.Model):
+    text_prompt = models.TextField()
+    answer = models.TextField()
+    decks = models.ManyToManyField(Deck, related_name="flashcards")
